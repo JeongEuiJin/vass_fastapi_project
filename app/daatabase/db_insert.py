16878cf77ee3d7fdd610614ff6efe192be518c4a
euijin <ejjeong@bud-on.com>
2022-10-07 11:52:58 +0900

    디비 insert 함수 추가 변경

--- app/daatabase/db_insert.py
+++ app/daatabase/db_insert.py
@@ -7,9 +7,13 @@ def ml_insert(results):
 
     print(results)
     for index, row in results.iterrows():
+        research_cursor.execute('''select id from dbo.VaccineHOIdefn where vaccine_id={} and hoidefn_id={}'''.format(row.vaccine, row.hoidefn))
+        vaccinehoidefn_id = research_cursor.fetchall()[0][0]
+
         research_cursor.execute(
-            'INSERT INTO dbo.AnalysisResultsML (studydesign, vaccine, vcntime, hoidefn, calculated_date, fi_ratio, injected_case, risk_case) values (?,?,?,?,?,?,?,?)',
-            row.studydesign, row.vaccine, row.vcntime, row.hoidefn, row.calculated_date, row.fi_ratio, row.injected_case, row.risk_case)
+            'INSERT INTO dbo.AnalysisResultsML (studydesign_id, vaccinehoidefn_id, calculated_date, fi_ratio, injected_case, risk_case) values (?,?,?,?,?,?)',
+            row.studydesign, vaccinehoidefn_id, row.calculated_date, row.fi_ratio, row.injected_case, row.risk_case
+        )
 
         research_cnxn.commit()
         research_cursor.close()
